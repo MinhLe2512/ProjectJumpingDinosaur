651f83c8dd6eabd50e7bdeb14cd3c6898a2a94ba
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2022-04-09 00:16:33 +0700

    Add player

--- js/utils/main_window.js
+++ js/utils/main_window.js
@@ -1,4 +1,4 @@
-let app;
+let app 
 
 window.onload = function() {
     app = new PIXI.Application(
@@ -8,9 +8,18 @@ window.onload = function() {
             backgroundColor: 0x34e5eb
         }
     );
-    document.body.appendChild(app.view)
+    document.body.appendChild(app.view);
+
+    player = new PIXI.Sprite.from('images/ezgif-4-0c1b2d3b5e-png-24x24-sprite-png/tile000.png');
+    player.anchor.set(0.5);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+
+    app.stage.addChild(player);
 }
 
 window.onresize = function(){
     app.renderer.resize(window.innerWidth, window.innerHeight);
-}
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2;
+};
