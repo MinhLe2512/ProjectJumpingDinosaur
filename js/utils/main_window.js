a5638917a7d1061803de7f77b926abdd5bc0dc62
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2022-04-09 20:08:14 +0700

    add jumping feature

--- js/utils/main_window.js
+++ js/utils/main_window.js
@@ -1,4 +1,8 @@
-let app 
+let app, player, jumpAt;
+let keys = {};
+
+let gravity = 1, direction = -1;
+let power = 20;
 
 window.onload = function() {
     app = new PIXI.Application(
@@ -15,7 +19,53 @@ window.onload = function() {
     player.x = app.view.width / 2;
     player.y = app.view.height / 2;
 
+
+    jumpAt = player.y;
+    //Mouse click interactions
+    app.view.addEventListener('click', playerJump);
+
+    window.addEventListener("keydown", function(e) {
+        keys[e.keyCode] = true;
+    });
+    window.addEventListener("keyup", function(e) {
+        keys[e.keyCode] = false;
+    });
+
     app.stage.addChild(player);
+    app.ticker.add(gameLoop);
+}
+
+
+let jumping = false;
+
+function playerJump() {  
+    if (jumping) return;
+    jumping = true;
+  
+    let time = 0;
+  
+    const tick = deltaMs => {
+      const jumpHeight = (-gravity / 2) * Math.pow(time, 2) + power * time;
+      console.log(jumpHeight)
+  
+      if (jumpHeight < 0) {
+        jumping = false;
+        PIXI.Ticker.shared.remove(tick);
+        player.y = jumpAt;
+        return;
+      }
+  
+      player.y = jumpAt + (jumpHeight * direction);
+      time += deltaMs;
+    }
+  
+    PIXI.Ticker.shared.add(tick);
+}
+
+function gameLoop() {
+    if (keys[32] == true) {
+        playerJump();
+    }
 }
 
 window.onresize = function(){
@@ -23,3 +73,4 @@ window.onresize = function(){
     player.x = app.view.width / 2;
     player.y = app.view.height / 2;
 };
+
